0c753c2c1db1a9b322bcd6c996b7234bb893009b
Yunsi Niu <niuyunsi@gmail.com>
2018-10-19 00:05:16 -0700

    15

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,13 +1,24 @@
 const Mutations = {
-    createDog(parent, args, ctx, info) {
-        global.dogs = global.dogs || [];
-        console.log(args);
-        // create a dog
-        const newDog = {
-            name: args.name
-        };
-        global.dogs.push(newDog);
-        return newDog;
+    // createDog(parent, args, ctx, info) {
+    //     global.dogs = global.dogs || [];
+    //     console.log(args);
+    //     // create a dog
+    //     const newDog = {
+    //         name: args.name
+    //     };
+    //     global.dogs.push(newDog);
+    //     return newDog;
+    // }
+    async createItem(parent, args, ctx, info) {
+        // TODO: Check if they ar logged in
+
+        const item = await ctx.db.mutation.createItem({
+            data: {
+                ...args
+            }
+        }, info);
+
+        return item;
     }
 };
 
