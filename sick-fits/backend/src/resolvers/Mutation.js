56587e18bd48c88b2e17d69fadb4444aaeedee1f
Yunsi Niu <niuyunsi@gmail.com>
2018-11-11 22:49:52 -0800

    20

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,25 +1,34 @@
 const Mutations = {
-    // createDog(parent, args, ctx, info) {
-    //     global.dogs = global.dogs || [];
-    //     console.log(args);
-    //     // create a dog
-    //     const newDog = {
-    //         name: args.name
-    //     };
-    //     global.dogs.push(newDog);
-    //     return newDog;
-    // }
-    async createItem(parent, args, ctx, info) {
-        // TODO: Check if they ar logged in
+  async createItem(parent, args, ctx, info) {
+    // TODO: Check if they ar logged in
 
-        const item = await ctx.db.mutation.createItem({
-            data: {
-                ...args
-            }
-        }, info);
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args
+        }
+      },
+      info
+    );
 
-        return item;
-    }
+    return item;
+  },
+  updateItem(parent, args, ctx, info) {
+    // take a copy of the updates
+    const updates = { ...args };
+    // remove th id from the updates
+    delete updates.id;
+    // run the update method;
+    return ctx.db.mutation.updateItem(
+      {
+        data: updates,
+        where: {
+          id: args.id
+        }
+      },
+      info
+    );
+  }
 };
 
 module.exports = Mutations;
--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -3,15 +3,8 @@ const {
 } = require('prisma-binding');
 
 const Query = {
-    // dogs(parent, args, ctx, info) {
-    //     global.dogs = global.dogs || [];
-    //     return global.dogs;
-    // }
-    // async items(parent, args, ctx, info) {
-    //     const items = await ctx.db.query.items();
-    //     return items
-    // }
-    items: forwardTo('db')
+    items: forwardTo('db'),
+    item: forwardTo('db')
 };
 
 module.exports = Query;
