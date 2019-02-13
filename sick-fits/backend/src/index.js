569e06bdf33c4b9895b5e77a0a62375ca88da960
Yunsi Niu <niuyunsi@gmail.com>
2019-02-12 21:27:24 -0800

    27

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,7 +1,8 @@
 const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 
 require('dotenv').config({
-    path: 'variables.env'
+  path: 'variables.env'
 });
 const createServer = require('./createServer');
 const db = require('./db');
@@ -9,14 +10,26 @@ const db = require('./db');
 const server = createServer();
 
 server.express.use(cookieParser());
-// TODO Use express middleware to populate current user
 
-server.start({
+// decode the JWT
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    req.userId = userId;
+  }
+  next();
+});
+
+server.start(
+  {
     cors: {
-        credentials: true,
-        origin: process.env.FRONTEND_URL,
-    },
-}, deets => {
+      credentials: true,
+      origin: process.env.FRONTEND_URL
+    }
+  },
+  deets => {
     console.log(`Server is now running on port
     http://localhost:${deets.port}`);
-})
+  }
+);
--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,11 +1,20 @@
-const {
-    forwardTo
-} = require('prisma-binding');
+const { forwardTo } = require('prisma-binding');
 
 const Query = {
-    items: forwardTo('db'),
-    item: forwardTo('db'),
-    itemsConnection: forwardTo('db')
+  items: forwardTo('db'),
+  item: forwardTo('db'),
+  itemsConnection: forwardTo('db'),
+  me(parent, args, ctx, info) {
+    if (!ctx.request.userId) {
+      return null;
+    }
+    return ctx.db.query.user(
+      {
+        where: { id: ctx.request.userId }
+      },
+      info
+    );
+  }
 };
 
 module.exports = Query;
