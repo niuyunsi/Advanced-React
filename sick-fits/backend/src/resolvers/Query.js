2c191e15327c04d99b3a7b535aed45d01d018d56
Yunsi Niu <niuyunsi@gmail.com>
2019-03-03 23:49:25 -0800

    35

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,4 +1,5 @@
 const { forwardTo } = require('prisma-binding');
+const { hasPermission } = require('../utils');
 
 const Query = {
   items: forwardTo('db'),
@@ -14,6 +15,16 @@ const Query = {
       },
       info
     );
+  },
+  async users(parent, args, ctx, info) {
+    // check login
+    if (!ctx.request.userId) {
+      throw new Error('You must be logged in');
+    }
+    // check permission
+    hasPermission(ctx.request.user, ['ADMIN', 'PERMISSIONUPDATE']);
+    // query users
+    return ctx.db.query.users({}, info);
   }
 };
 
