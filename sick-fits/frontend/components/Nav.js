a52acc82b88f11aeb4e1d99c725b87bb5147de45
Yunsi Niu <niuyunsi@gmail.com>
2019-05-19 23:29:06 -0700

    45

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -1,9 +1,11 @@
 import Link from 'next/link';
 import { Mutation } from 'react-apollo';
+
 import { TOGGLE_CART_MUTATION } from './Cart';
 import NavStyles from './styles/NavStyles';
 import User from './User';
 import Signout from './Signout';
+import CartCount from './CartCount';
 
 const Nav = () => (
   <User>
@@ -25,7 +27,12 @@ const Nav = () => (
             </Link>
             <Signout />
             <Mutation mutation={TOGGLE_CART_MUTATION}>
-              {toggleCart => <button onClick={toggleCart}>My cart</button>}
+              {toggleCart => (
+                <button onClick={toggleCart}>
+                  My cart
+                  <CartCount count={me.cart.reduce((tally, cartItem) => tally + cartItem.quantity, 0)} />
+                </button>
+              )}
             </Mutation>
           </>
         )}
