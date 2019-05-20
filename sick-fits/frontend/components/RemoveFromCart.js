d144d3e816e0208cfca2618a3a82ee6ef9290f0e
Yunsi Niu <niuyunsi@gmail.com>
2019-05-19 23:10:09 -0700

    44

--- sick-fits/frontend/components/RemoveFromCart.js
+++ sick-fits/frontend/components/RemoveFromCart.js
@@ -27,9 +27,32 @@ class RemoveFromCart extends React.Component {
   static propTypes = {
     id: PropTypes.string.isRequired
   };
+
+  // called when mutation comes back
+  update = (cache, payload) => {
+    // read the cache
+    const data = cache.readQuery({ query: CURRENT_USER_QUERY });
+    // remove that item from the cart
+    const cartItemId = payload.data.removeFromCart.id;
+    data.me.cart = data.me.cart.filter(cartItem => cartItem.id !== cartItemId);
+    // write it back to cache
+    cache.writeQuery({ query: CURRENT_USER_QUERY, data });
+  };
+
   render() {
     return (
-      <Mutation mutation={REMOVE_FROM_CART_MUTATION} variables={{ id: this.props.id }}>
+      <Mutation
+        mutation={REMOVE_FROM_CART_MUTATION}
+        variables={{ id: this.props.id }}
+        update={this.update}
+        optimisticResponse={{
+          __typename: 'Mutation',
+          removeFromCart: {
+            __typename: 'CartItem',
+            id: this.props.id
+          }
+        }}
+      >
         {(removeFromCart, { loading, error }) => (
           <BigButton
             title="delete item"
