5440f025a67ff43918126671a3ad4223c4051ccf
Yunsi Niu <niuyunsi@gmail.com>
2019-05-19 22:44:43 -0700

    43

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -239,6 +239,32 @@ const Mutations = {
       },
       info
     );
+  },
+  async removeFromCart(parent, args, ctx, info) {
+    // find the cart item
+    const cartItem = await ctx.db.query.cartItem(
+      {
+        where: {
+          id: args.id
+        }
+      },
+      `{id, user { id }}`
+    );
+    // make sure we found an item
+    if (!cartItem) throw new Error('No CartItem Found');
+    // make sure they own that cart item
+    if (cartItem.user.id !== ctx.request.userId) {
+      throw new Error('does not own this item');
+    }
+    // delete cart item
+    return ctx.db.mutation.deleteCartItem(
+      {
+        where: {
+          id: args.id
+        }
+      },
+      info
+    );
   }
 };
 
--- sick-fits/frontend/components/CartItem.js
+++ sick-fits/frontend/components/CartItem.js
@@ -3,6 +3,8 @@ import formatMoney from '../lib/formatMoney';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
+import RemoveFromCart from './RemoveFromCart';
+
 const CartItemStyles = styled.li`
   padding: 1rem 0;
   border-bottom: 1px solid ${props => props.theme.lightgrey};
@@ -31,6 +33,7 @@ const CartItem = ({ cartItem }) => (
         </em>
       </p>
     </div>
+    <RemoveFromCart id={cartItem.id} />
   </CartItemStyles>
 );
 
